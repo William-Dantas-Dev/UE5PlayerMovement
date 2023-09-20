14c46730a5e75bc60180e94b613fce3f59c83c2e
William-Dantas-Dev <shiroe.gaming.dev@gmail.com>
2023-09-20 19:41:34 -0300

    Initial commit

---
+++ Source/UE5PlayerMovement/UE5PlayerMovement.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5PlayerMovement.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5PlayerMovement, "UE5PlayerMovement" );
